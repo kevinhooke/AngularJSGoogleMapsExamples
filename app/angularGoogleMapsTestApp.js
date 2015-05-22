1e9cd5ad450d90eb96c2beca5fdbc66752f698eb
Kevin Hooke <kevin.hooke@gmail.com>
2015-05-22 15:14:12 -0700

    fixed Angular Google Maps example - completed marker add/remove logic

--- app/angularGoogleMapsTestApp.js
+++ app/angularGoogleMapsTestApp.js
@@ -1,31 +1,43 @@
 'use strict';
 
 var app = angular.module('angularGoogleMapsTestApp', ['uiGmapgoogle-maps']);
-    var marker1 = {id: 1, latitude: 37.7699298, longitude: -122.40};
-    var marker2 = {id: 2, latitude: 37.7699298, longitude: -122.45};
-    var marker3 = {id: 3, latitude: 37.7699298, longitude: -122.46};
-    var inc = 0.01;
+    //one initial marker
+    var marker1 = {id: 1, latitude: 37.769, longitude: -122.44};
+
+    var markersDisplayed = 0;
+    var markersAdded = 0;
+    var maxMarkersToDisplay = 3;
+    var increment = 0.02;
+    var startingLongitude = -122.44;
+    var startingLatitude = 37.769;
     
 app.controller('angularGoogleMapCtrl', function ($scope) {
 
     $scope.map = {center: {latitude: 37.7699298, longitude: -122.4469157}, zoom: 12};
     $scope.markers = [];
-
-    $scope.markers[0] = marker1;
-    $scope.markers[1] = marker2;
-    $scope.markers[2] = marker3;
+    
+    $scope.removeMarkers = function () {
+        $scope.markers = [];
+        markersDisplayed = 0;
+        markersAdded = 0;
+    }
 
     $scope.toggleAddAndRemoveMarkers = function () {
-        if ($scope.markers.length > 0) {
-            $scope.markers = [];
-        }
-        else {
-            $scope.markers[0] = marker1;
-            $scope.markers[1] = marker2;
-            $scope.markers[2] = marker3;
-        }
-        ;
 
-    }
+        //if 3 displayed already, pop first one off of the array
+        if(markersDisplayed >= 3) {
+            markersDisplayed--;
+            $scope.markers.shift();
+        }
+        //addd a new position
+        markersDisplayed++;
+        $scope.markers.push(
+            {
+                id: markersAdded,
+                latitude: startingLatitude, 
+                longitude: (startingLongitude + (markersAdded * increment))
+            });
+        markersAdded++;
+        };
 
 });
